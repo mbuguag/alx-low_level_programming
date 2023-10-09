703ab27edb99debace73a3bfb70340dbc3c18399
mbuguag <mbuguajoseph498@gmail.com>
2023-10-09 16:07:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: success 0
+ */
+int main(void)
+{
+	char jm;
+
+	for (jm = 'a'; jm <= 'z'; jm++)
+		if (jm != e && jm != q)
+			putchar(jm);
+	putchar('\n');
+	return (0);
+}
