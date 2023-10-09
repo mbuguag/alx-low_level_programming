386d94118b63348fb7f93b317d5efb75b63c45fe
mbuguag <mbuguajoseph498@gmail.com>
2023-10-09 16:45:59 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ /**
+  * main - main function
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
